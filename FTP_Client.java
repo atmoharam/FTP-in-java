72394e5db3cf47d02baf713486843040ce92d7d0
zwedy <zwedymoi@gmail.com>
2021-01-20 21:52:17 +0200

    image

--- FTP_Client.java
+++ FTP_Client.java
@@ -1,1 +1,14 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.*;
 
+class Client {
+    public static void main ( String[] args ) throws Exception {
+        try ( Socket socket = new Socket ( "localHost", 25000 ) ) {
+            BufferedImage image = ImageIO.read ( socket.getInputStream ( ) );
+            File down = new File ( "E:\\ahmed.jpg" );
+            ImageIO.write ( image, "jpg", down );
+        }
+    }
+}
--- FTP_Server.java
+++ FTP_Server.java
@@ -1,1 +1,17 @@
-edited by zwedy
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.*;
+
+public class Server {
+    public static void main ( String[] args ) throws Exception {
+        File imo = new File ( "D:\\New folder\\tito.jpg" );
+        BufferedImage image = ImageIO.read ( imo );
+        try ( ServerSocket serv = new ServerSocket ( 25000 ) ) {
+            try ( Socket socket = serv.accept ( ) ) {
+                ImageIO.write ( image , "jpg" , socket.getOutputStream ( ) );
+
+            }
+        }
+    }
+}
