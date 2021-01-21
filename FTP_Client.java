2712fe108dc76192730737e46a7c567452d2034a
mido_ <atmoharm>
2021-01-21 10:57:25 +0200

    vedio

--- FTP_Client.java
+++ FTP_Client.java
@@ -1,3 +1,6 @@
+/////////////////////// image
+
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -9,6 +12,34 @@ class Client {
             BufferedImage image = ImageIO.read ( socket.getInputStream ( ) );
             File down = new File ( "E:\\ahmed.jpg" );
             ImageIO.write ( image, "jpg", down );
+        }
+    }
+}
+
+
+
+////////////////// Vedio
+
+import javax.imageio.ImageIO;
+        import java.awt.image.BufferedImage;
+        import java.io.DataInputStream;
+        import java.io.File;
+        import java.io.FileOutputStream;
+        import java.net.*;
+
+class client {
+    public static void main ( String[] args ) throws Exception {
+        try ( Socket socket = new Socket ( "localHost", 25000 ) ) {
+
+            File nf = new File("D:/K.mp4");
+            FileOutputStream fw = new FileOutputStream(nf);
+            DataInputStream dIn = new DataInputStream(socket.getInputStream());
+            int length = dIn.readInt();                    // read length of incoming message
+            if(length>0) {
+                byte[] message = new byte[length];
+                dIn.readFully(message, 0, message.length); // read the message
+                fw.write ( message );
+            }
         }
     }
 }
--- FTP_Server.java
+++ FTP_Server.java
@@ -1,3 +1,4 @@
+////////////////image
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -15,3 +16,30 @@ public class server {
         }
     }
 }
+
+
+///////////////////////vedio
+import javax.imageio.ImageIO;
+        import java.awt.image.BufferedImage;
+        import java.io.DataOutputStream;
+        import java.io.File;
+        import java.io.FileInputStream;
+        import java.io.ObjectInputStream;
+        import java.net.*;
+
+public class server {
+    public static void main ( String[] args ) throws Exception {
+        File file = new File("D:\\op.mp4");
+        FileInputStream fin = new FileInputStream(file);
+        byte b[] = new byte[(int)file.length()];
+        fin.read(b);
+        try ( ServerSocket serv = new ServerSocket ( 25000 ) ) {
+            try ( Socket socket = serv.accept ( ) ) {
+                DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+                dOut.writeInt(b.length); // write length of the message
+                dOut.write(b);
+            }
+        }
+    }
+}
+
