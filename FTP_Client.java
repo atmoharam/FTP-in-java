5c95530b2bf8bd7a7d5980b6571f4f5f5f8440f8
mido_ <atmoharm>
2021-01-21 10:57:25 +0200

    Send/rec data

--- FTP_Client.java
+++ FTP_Client.java
@@ -1,14 +1,28 @@
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.net.*;
 
-class Client {
+class client {
     public static void main ( String[] args ) throws Exception {
-        try ( Socket socket = new Socket ( "localHost", 25000 ) ) {
-            BufferedImage image = ImageIO.read ( socket.getInputStream ( ) );
-            File down = new File ( "E:\\ahmed.jpg" );
-            ImageIO.write ( image, "jpg", down );
+        try ( Socket socket = new Socket ( "localHost" , 25000 ) ) {
+            //create a new file to store data
+            File DownloadedFile = new File ( "D:/K.jpg" );
+            //create file output stream to write data in file
+            FileOutputStream FileWriter = new FileOutputStream ( DownloadedFile );
+            //create data stream to read data from socket
+            DataInputStream DataDownloaded = new DataInputStream ( socket.getInputStream ( ) );
+            //read the length of data
+            int length = DataDownloaded.readInt ( );
+            //if no data sent will do nothing
+            if ( length > 0 ) {
+                //create array of bytes to store data with length
+                byte[] DataBytes = new byte[ length ];
+                //read the bytes of data
+                DataDownloaded.readFully ( DataBytes , 0 , DataBytes.length );
+                //write data from array to file
+                FileWriter.write ( DataBytes );
+            }
         }
     }
 }
--- FTP_Server.java
+++ FTP_Server.java
@@ -1,16 +1,25 @@
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.net.*;
 
 public class server {
     public static void main ( String[] args ) throws Exception {
-        File imo = new File ( "D:\\New folder\\tito.jpg" );
-        BufferedImage image = ImageIO.read ( imo );
+        //create a file of any ex (jpg, mp3 , mp4 , txt , pdf)
+        File file = new File ( "D:\\op.jpg" );
+        //create a file input stream to read the file content
+        FileInputStream FileReader = new FileInputStream ( file );
+        //create an array of byte with file length
+        byte DataBytes[] = new byte[ ( int ) file.length ( ) ];
+        //read the data and put data in array as bytes
+        FileReader.read ( DataBytes );
         try ( ServerSocket serv = new ServerSocket ( 25000 ) ) {
             try ( Socket socket = serv.accept ( ) ) {
-                ImageIO.write ( image , "jpg" , socket.getOutputStream ( ) );
-
+                //create a Data out put stream and pass the output to socket stream
+                DataOutputStream DataOut = new DataOutputStream ( socket.getOutputStream ( ) );
+                //write length of data and data in socket stream
+                DataOut.writeInt ( DataBytes.length );
+                DataOut.write ( DataBytes );
             }
         }
     }
