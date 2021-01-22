2d107ea898ea10aa88c88d9bcb3194b34eba6b3c
fatema373 <77742944+fatema373@users.noreply.github.com>
2021-01-22 14:17:20 +0200

    Update FTP_Client.java

--- FTP_Client.java
+++ FTP_Client.java
@@ -1,28 +1,36 @@
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.net.*;
+package tcp_chat_20190373;
 
-class client {
-    public static void main ( String[] args ) throws Exception {
-        try ( Socket socket = new Socket ( "localHost" , 25000 ) ) {
-            //create a new file to store data
-            File DownloadedFile = new File ( "D:/K.jpg" );
-            //create file output stream to write data in file
-            FileOutputStream FileWriter = new FileOutputStream ( DownloadedFile );
-            //create data stream to read data from socket
-            DataInputStream DataDownloaded = new DataInputStream ( socket.getInputStream ( ) );
-            //read the length of data
-            int length = DataDownloaded.readInt ( );
-            //if no data sent will do nothing
-            if ( length > 0 ) {
-                //create array of bytes to store data with length
-                byte[] DataBytes = new byte[ length ];
-                //read the bytes of data
-                DataDownloaded.readFully ( DataBytes , 0 , DataBytes.length );
-                //write data from array to file
-                FileWriter.write ( DataBytes );
-            }
-        }
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+public class Client {
+    
+   Client() throws UnknownHostException, IOException{
+       Scanner in=new Scanner (System.in);
+          try (Socket socket = new Socket (InetAddress.getLocalHost(),28000)){
+              DataInputStream ServerReadSource = new  DataInputStream(socket.getInputStream());
+              DataOutputStream ServerWriteSource = new DataOutputStream(socket.getOutputStream());
+              String message = ServerReadSource.readUTF();
+              System.out.println(message);
+              String UserName = in.next();
+              ServerWriteSource.writeUTF(UserName);
+              message = ServerReadSource.readUTF();
+              System.out.println(message);
+              UserName = in.next();
+              ServerWriteSource.writeUTF(UserName);
+              message = ServerReadSource.readUTF();
+              System.out.println(message);
+          }
+        
     }
-}
+
+   }
