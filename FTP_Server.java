52806cb9b2b2cc746646a4ef50ecf045ccd531f5
fatema373 <77742944+fatema373@users.noreply.github.com>
2021-01-22 14:21:35 +0200

    Update FTP_Server.java

--- FTP_Server.java
+++ FTP_Server.java
@@ -1,26 +1,76 @@
+
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.net.*;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class Server {
+    
+     Server() throws IOException{
+        try(ServerSocket server = new ServerSocket(28000)){
+            Socket client = server.accept();
+            try{
+                
+                String [] dataOnLine = new String [8];
+                int line = 0;
+                try{
+                    File myObj = new File("C:\\Users\\Lenovo 500\\Documents\\data\\Data of users.txt"); // craeted a file object named FileName
+                    Scanner MyReader = new Scanner(myObj); 
+                     while(MyReader.hasNextLine()){
 
-public class server {
-    public static void main ( String[] args ) throws Exception {
-        //create a file of any ex (jpg, mp3 , mp4 , txt , pdf)
-        File file = new File ( "D:\\op.jpg" );
-        //create a file input stream to read the file content
-        FileInputStream FileReader = new FileInputStream ( file );
-        //create an array of byte with file length
-        byte DataBytes[] = new byte[ ( int ) file.length ( ) ];
-        //read the data and put data in array as bytes
-        FileReader.read ( DataBytes );
-        try ( ServerSocket serv = new ServerSocket ( 25000 ) ) {
-            try ( Socket socket = serv.accept ( ) ) {
-                //create a Data out put stream and pass the output to socket stream
-                DataOutputStream DataOut = new DataOutputStream ( socket.getOutputStream ( ) );
-                //write length of data and data in socket stream
-                DataOut.writeInt ( DataBytes.length );
-                DataOut.write ( DataBytes );
+                            String info =MyReader.nextLine();
+                            dataOnLine[line] = info;
+                            line++;
+                 }
+
+                }
+               catch(FileNotFoundException e)
+			{
+			System.out.println("FILE NOT FOUNDED");
+			}
+           
+                
+                    DataInputStream clientReadSource = new  DataInputStream(client.getInputStream());
+                    DataOutputStream clientWriteSource = new DataOutputStream(client.getOutputStream());
+                    clientWriteSource.writeUTF("Please, Enter Username:");
+                    String request1 = clientReadSource.readUTF();
+                    int dummy = 0 ;
+                    String request2 = "";
+                    
+                    for(int i=0 ; i<8 ; i++){
+                        if(request1.equals( dataOnLine[i] )){
+                            request2 = dataOnLine[i+1];
+                            dummy=1;
+                            break;
+                        }
+                    }
+                    if(dummy == 0){
+                        clientWriteSource.writeUTF("Login Failed and the connection will terminate");
+                        client.close();
+                    }
+                    clientWriteSource.writeUTF("Please Enter the password");
+                    request1 = clientReadSource.readUTF();
+                    if(request1.equals(request2)){
+                         clientWriteSource.writeUTF("Login Successfully");
+                    }
+                    else{
+                        clientWriteSource.writeUTF("Login Failed and the connection will terminate");
+                        client.close();
+                    }
+
             }
-        }
-    }
-}
+         catch(Exception e)
+         { e.printStackTrace();}
+        
+           }
+      
+        }}
+    
+
