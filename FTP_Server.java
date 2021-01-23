70652c88e04c9bcf88229de9350483d4fac42682
fatema373 <77742944+fatema373@users.noreply.github.com>
2021-01-23 22:19:37 +0200

    Update FTP_Server.java

--- FTP_Server.java
+++ FTP_Server.java
@@ -1,71 +1,78 @@
 
+
 package server_side;
 
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import static java.util.Collections.list;
 import java.util.Scanner;
 
 
 
 public class Server_side {
 
-
+   
     public static void main(String[] args) throws IOException {
-        try(ServerSocket server = new ServerSocket(28000)){
+       try(ServerSocket server = new ServerSocket(28000)){
             Socket client = server.accept();
             try{
-
+                
                 String [] dataOnLine = new String [8];
                 int line = 0;
                 try{
-                    File myObj = new File("D:\\ftp\\Data_of_users.txt"); // craeted a file object named FileName
-                    Scanner MyReader = new Scanner(myObj);
-                    while(MyReader.hasNextLine()){
+                    File myObj = new File("C:\\Users\\Lenovo 500\\Documents\\data\\Data of users.txt"); // craeted a file object named FileName
+                    Scanner MyReader = new Scanner(myObj); 
+                     while(MyReader.hasNextLine()){
 
-                        String info =MyReader.nextLine();
-                        dataOnLine[line] = info;
-                        line++;
-                    }
+                            String info =MyReader.nextLine();
+                            dataOnLine[line] = info;
+                            line++;
+                 }
 
                 }
-                catch(FileNotFoundException e)
-                {
-                    System.out.println("FILE NOT FOUNDED");
-                }
-
-
+               catch(FileNotFoundException e)
+			{
+			System.out.println("FILE NOT FOUNDED");
+			}
+           
+                
                 DataInputStream clientReadSource = new  DataInputStream(client.getInputStream());
                 DataOutputStream clientWriteSource = new DataOutputStream(client.getOutputStream());
                 clientWriteSource.writeUTF("Please, Enter Username:");
                 String request1 = clientReadSource.readUTF();
-                String user=request1;
                 int dummy = 0 ;
+                String user = request1;
                 String request2 = "";
                 String userName = "";
 
                 for(int i=0 ; i<8 ; i++){
-                    if(request1.equals( dataOnLine[i] )){
-                        request2 = dataOnLine[i+1];
-                        userName = request1;
-                        dummy=1;
-                        break;
+                      if(request1.equals( dataOnLine[i] )){
+                            request2 = dataOnLine[i+1];
+                            userName = request1;
+                            dummy=1;
+                            break;
+                        }
                     }
-                }
                 if(dummy == 0){
-                    clientWriteSource.writeUTF("Login Failed and the connection will terminate");
-                    client.close();
-                }
+                        clientWriteSource.writeUTF("Login Failed and the connection will terminate");
+                        client.close();
+                    }
                 clientWriteSource.writeUTF("Please Enter the password");
                 request1 = clientReadSource.readUTF();
                 if(request1.equals(request2)){
-                    clientWriteSource.writeUTF("Login Successfully");
-                }
+                         clientWriteSource.writeUTF("Login Successfully");
+                    }
                 else{
-                    clientWriteSource.writeUTF("Login Failed and the connection will terminate");
-                    client.close();
-                }
-                String msg=clientReadSource.readUTF();
+                        clientWriteSource.writeUTF("Login Failed and the connection will terminate");
+                        client.close();
+                    }
+               String msg=clientReadSource.readUTF();
                 System.out.println(msg);
                 File file=new File("E:\\FTP"+user);
                 String[] files=file.list();
@@ -75,7 +82,11 @@ public class Server_side {
                 }
                 msg=clientReadSource.readUTF();
                 System.out.println(msg);
-                File file2=new File("E:\\FTP"+user+msg);
+                
+                
+                
+                System.out.println(msg);
+                File file2=new File("C:\\Users\\Lenovo 500\\Documents\\FTP\\"+user+msg);
                 String[] files2=file2.list();
                 clientWriteSource.writeUTF(String.valueOf(files2.length));
                 for (String string: files2) {
@@ -99,25 +110,32 @@ public class Server_side {
                     msgg=msg2+".jpeg";
                 }
                 clientWriteSource.writeUTF(msgg);
-                File file3=new File("E:\\FTP"+user+msg+msgg);
+                //Crearte a file of any ex(jpg, mp3, mp4, txt, pdf)
+                File file3=new File("C:\\Users\\Lenovo 500\\Documents\\FTP\\"+user+msg+msgg);
+                //Create a file input stream to read a file content
                 FileInputStream filereader=new FileInputStream(file3);
+                //Create an array of byte with file length
                 byte Databytes[] =new byte[(int) file3.length()];
+                //read the data and put data in arrray as bytes
                 filereader.read(Databytes);
                 try (ServerSocket serv=new ServerSocket(25000)){
                     try (Socket socket=serv.accept()){
+                        //Create data output stream and pass the output to socket stream
                         DataOutputStream dataout=new DataOutputStream(socket.getOutputStream());
+                        //Write length of data and data in socket stream
                         dataout.writeInt(Databytes.length);
                         dataout.write(Databytes);
                     }
                 }
+
             }
-            catch(Exception e)
-            { e.printStackTrace();}
-
+         catch(Exception e)
+         { e.printStackTrace();}
+        
+           }
+      
         }
-
-    }
-
+    
 }
 
 
