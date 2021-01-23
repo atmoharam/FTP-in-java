78883c1ef76a631543928e2e7b8c23dae67c03e9
at.moharm <62400652+atmoharam@users.noreply.github.com>
2021-01-24 00:08:12 +0200

    Update FTP_Client.java

--- FTP_Client.java
+++ FTP_Client.java
@@ -1,72 +1,58 @@
-
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-
-public class Client {
-    String temp= " ";
-
-    Client() throws UnknownHostException, IOException{
-        Scanner in=new Scanner (System.in);
-        try (Socket socket = new Socket (InetAddress.getLocalHost(),28000)){
-            DataInputStream ServerReadSource = new  DataInputStream(socket.getInputStream());
-            DataOutputStream ServerWriteSource = new DataOutputStream(socket.getOutputStream());
-            String message = ServerReadSource.readUTF();
-            System.out.println(message);
-            String UserName = in.next();
-            ServerWriteSource.writeUTF(UserName);
-            message = ServerReadSource.readUTF();
-            System.out.println(message);
-            UserName = in.next();
-            ServerWriteSource.writeUTF(UserName);
-            message = ServerReadSource.readUTF();
-            System.out.println(message);
-            ServerWriteSource.writeUTF("show my directories");
-            ServerReadSource.readUTF();
-            String str=in.nextLine();
-            str.trim();
-            ServerWriteSource.writeUTF(str);
-            int l=0;
-            l=ServerReadSource.readInt();
-            for (int i=0;i<l;i++){
-                String d=ServerReadSource.readUTF();
-                System.out.println(d);
+public class client {
+    public static void main ( String[] args ) throws IOException {
+        String temp = " ";
+        Scanner in = new Scanner ( System.in );
+        try ( Socket socket = new Socket ( InetAddress.getLocalHost ( ), 28000 ) ) {
+            DataInputStream ServerReadSource = new DataInputStream ( socket.getInputStream ( ) );
+            DataOutputStream ServerWriteSource = new DataOutputStream ( socket.getOutputStream ( ) );
+            String message = ServerReadSource.readUTF ( );
+            System.out.println ( message );
+            String UserName = in.next ( );
+            ServerWriteSource.writeUTF ( UserName );
+            message = ServerReadSource.readUTF ( );
+            System.out.println ( message );
+            UserName = in.next ( );
+            ServerWriteSource.writeUTF ( UserName );
+            message = ServerReadSource.readUTF ( );
+            System.out.println ( message );
+            ServerWriteSource.writeUTF ( "show my directories" );
+            int l;
+            l = ServerReadSource.readInt ( );     //to store n.o dirs
+            for ( int i = 0 ; i < l ; i++ ) {
+                String d = ServerReadSource.readUTF ( );
+                System.out.println ( d );
             }
-            String kind=in.nextLine();
-            kind.trim();
-            ServerWriteSource.writeUTF(kind);
-            l=0;
-            l=ServerReadSource.readInt();
-            for (int i=0;i<l;i++){
-                String d=ServerReadSource.readUTF();
-                System.out.println(d);
+            String kind = in.next ( );
+            ServerWriteSource.writeUTF ( kind );
+            l = ServerReadSource.readInt ( );
+            for ( int i = 0 ; i < l ; i++ ) {
+                String d = ServerReadSource.readUTF ( );
+                System.out.println ( d );
             }
-            String name=in.nextLine();
-            ServerWriteSource.writeUTF(name);
-            temp= ServerReadSource.readUTF();
-
+            String name = in.next ( );
+            ServerWriteSource.writeUTF ( name );
+            temp = ServerReadSource.readUTF ( );
+            socket.close ( );
+            ServerReadSource.close ( );
+            ServerWriteSource.close ( );
         }
-
-        try (Socket socket=new Socket("localHost",25000)){
-
-            File DownloadedFile=new File("D:\\ftp2"+temp);
-            FileOutputStream FileWriter=new FileOutputStream(DownloadedFile);
-            DataInputStream DataDownloaded=new DataInputStream(socket.getInputStream());
-            int length=DataDownloaded.readInt();
-            if (length>0){
-                byte[] DataBytes=new byte[length];
-                DataDownloaded.readFully(DataBytes,0,DataBytes.length);
-                FileWriter.write(DataBytes);
-            }
-
+        Socket socket2 = new Socket ( InetAddress.getLocalHost ( ), 25188 );
+        File DownloadedFile = new File ( "D:\\ftp2\\" + temp );
+        FileOutputStream FileWriter = new FileOutputStream ( DownloadedFile );
+        DataInputStream DataDownloaded = new DataInputStream ( socket2.getInputStream ( ) );
+        int length = DataDownloaded.readInt ( );
+        if ( length > 0 ) {
+            byte[] DataBytes = new byte[ length ];
+            DataDownloaded.readFully ( DataBytes, 0, DataBytes.length );
+            FileWriter.write ( DataBytes );
         }
+        socket2.close ( );
+        FileWriter.close ( );
+        DataDownloaded.close ( );
     }
-
-
-
 }
